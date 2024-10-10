0d3f51a358b19067f16c084fa407cb95dcf4c40d
FurtiveThomas <84189767+FurtiveThomas@users.noreply.github.com>
2024-10-10 16:45:10 -0400

    Update main.js

--- Cookie Clicker ( 2.054 )/main.js
+++ Cookie Clicker ( 2.054 )/main.js
@@ -1,14 +1,4 @@
-﻿/*
-All this code is copyright Orteil, 2013-2023.
-	-with some help, advice and fixes by Nicholas Laux, Debugbro, Opti, the folks at Playsaurus, and lots of people on reddit, Discord, and the DashNet forums
-	-also includes a bunch of snippets found on stackoverflow.com and others
-	-want to mod the game? scroll down to the "MODDING API" section
-Hello, and welcome to the joyous mess that is main.js. Code contained herein is not guaranteed to be good, consistent, or sane. Most of this is years old at this point and harkens back to simpler, cruder times. In particular I've tried to maintain compatibility with fairly old versions of javascript, which means luxuries such as 'let', arrow functions and string literals are unavailable.
-As Cookie Clicker is rife with puns and tricky wordplay, localization was never intended to be possible - but ended up happening anyway as part of the Steam port. As a result, usage of strings is somewhat unorthodox in some places.
-Have a nice trip, and stay safe.
-Spoilers ahead.
-http://orteil.dashnet.org
-*/
+﻿
 
 /*=====================================================================================
 MISC HELPER FUNCTIONS
